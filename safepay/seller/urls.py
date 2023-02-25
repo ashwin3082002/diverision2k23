ad843617a26d8b34ed6158d8fb6b00a1c81764cb
Ashwin R <ashwin3082002@gmail.com>
2023-02-25 23:15:38 +0530

    Created Apps
    Created apps and setup the Database scheme

--- safepay/seller/urls.py
+++ safepay/seller/urls.py
@@ -13,22 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.index, name='index'),
-    path('seller/', include('seller.urls')),
-    path('buyer/', include('buyer.urls')),
+    path('',views.seller_index, name='seller_index'),
 ]
 
-# Serving the media files in development mode
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += staticfiles_urlpatterns()
