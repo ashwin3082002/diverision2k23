4c8426f0830ffcb43d925a44b4148140afc3e417
Ashwin R <ashwin3082002@gmail.com>
2023-02-26 11:43:07 +0530

    changes

--- buyer/urls.py
+++ buyer/urls.py
@@ -18,4 +18,11 @@ from . import views
 
 urlpatterns = [
     path('',views.buyer_index, name='buyer_index'),
+    path('buyer_login/',views.buyer_login, name='buyer_login'),
+    path('cart/',views.cart_page, name='cart_page'),
+    path('checkout/',views.checkout_page, name='checkout_page'),
+    path('home/',views.home_page, name='home_page'),
+    path('orderconfirm/',views.orderconfirm_page, name='orderconfirm_page'),
+    path('orders/',views.orders_page, name='orders_page'),
+    path('wallet/',views.wallet_page, name='wallet_page'),
 ]
--- buyer/views.py
+++ buyer/views.py
@@ -2,4 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 def buyer_index(request):
-    return render(request, 'buyer/index.html')
+    return render(request, 'buyer/home.html')
+
+def buyer_login(request):
+    return render(request, 'buyer/login.html')
+
+def cart_page(request):
+    return render(request, 'buyer/cart.html')
+
+def checkout_page(request):
+    return render(request, 'buyer/checkout.html')
+
+def home_page(request):
+    return render(request, 'buyer/home.html')
+
+def orderconfirm_page(request):
+    return render(request, 'buyer/orderconfirm.html')
+
+def orders_page(request):
+    return render(request, 'buyer/orders.html')
+
+def wallet_page(request):
+    return render(request, 'buyer/wallet.html')
